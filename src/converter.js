4df1df633c0852290ada0857febf35546701a6d7
Attila Szeremi <attila@szeremi.org>
2017-12-28 20:46:52 +0100

    Generate commands exact to the dataset

--- src/converter.js
+++ src/converter.js
@@ -5,7 +5,7 @@
  * @return {Array}
  */
 function getMatches(sourceString) {
-  const pattern = /\s+\d+\) ([\w\\\\:]+)/g;
+  const pattern = /\s+\d+\) ([\w\\\\:]+( with data set [^\s]+)?)/g;
   let match;
   const matches = [];
   // eslint-disable-next-line no-cond-assign
@@ -15,12 +15,54 @@ function getMatches(sourceString) {
   return matches;
 }
 
+/**
+ * Returns a string joined with a pipe (|) and surrounded by parentheses for regex.
+ * If there is only one string, it does not surround the string with parentheses.
+ *
+ * @param {String[]} stringArray
+ * @return String
+ */
+function joinRegex(stringArray) {
+  return stringArray.length > 1 ?
+    `(${stringArray.join('|')})` :
+    stringArray[0];
+}
+
 function convertMatchesToPhpStormArgument(matches) {
+  const classes = {};
+  matches.forEach(match => {
+    const { _class, method, dataset } = getMatchParts(match);
+    if (!classes[_class]) {
+      classes[_class] = {};
+    }
+    const savedClassMethods = classes[_class];
+    if (!savedClassMethods[method]) {
+      savedClassMethods[method] = [];
+    }
+    const savedMethodDatasets = savedClassMethods[method];
+    savedMethodDatasets.push(dataset);
+  });
+  const tests = Object.keys(classes).map(className => {
+    return className + groupMethods(classes[className]);
+  });
   const escapeBackslahes = input => input.replace(/\\/g, '\\\\');
   return matches.length ?
-    `--filter="/^(${escapeBackslahes(matches.join('|'))})/"` :
+    `--filter "/${escapeBackslahes(joinRegex(tests))}$/"` :
     '';
 }
 
-export { getMatches, convertMatchesToPhpStormArgument };
+function getMatchParts(match) {
+  const [, _class, method, dataset] = match.match(/^([^:]+::)([^ ]+)( with data set .*)?$/);
+  return { _class, method, dataset: dataset || '' };
+}
+
+function groupMethods(methodKeyObject) {
+  const asArray = Object.keys(methodKeyObject).map(method => method + joinRegex(methodKeyObject[method]));
+  return joinRegex(asArray);
+}
+
+/** Methods not to be used, but exposed for testing purposes */
+const test = { getMatchParts, groupMethods };
+
+export { getMatches, convertMatchesToPhpStormArgument, test};
 
--- test/unit/specs/converter.spec.js
+++ test/unit/specs/converter.spec.js
@@ -1,5 +1,5 @@
 import bigExample from './converter_big_example.txt';
-import { convertMatchesToPhpStormArgument, getMatches } from '../../../src/converter';
+import { convertMatchesToPhpStormArgument, getMatches, test } from '../../../src/converter';
 
 const simpleExample = `
 9) amcsi\\playground\\test\\Class6Test::test3
@@ -13,6 +13,18 @@ This test did not perform any assertions
 
 `;
 
+const groupedExample = `
+9) amcsi\\playground\\test\\Class6Test::test1
+This test did not perform any assertions
+
+C:\\Users\\attila\\AppData\\Roaming\\Composer\\vendor\\phpunit\\phpunit\\src\\TextUI\\Command.php:212
+C:\\Users\\attila\\AppData\\Roaming\\Composer\\vendor\\phpunit\\phpunit\\src\\TextUI\\Command.php:141
+
+10) amcsi\\playground\\test\\Class6Test::test2
+This test did not perform any assertions
+
+`;
+
 describe('converter', () => {
   it('should get the list of matches from a source string', () => {
     const result = getMatches(simpleExample);
@@ -21,9 +33,43 @@ describe('converter', () => {
     expect(result[1]).to.equal('amcsi\\playground\\test\\Class7Test::test1');
   });
 
+  it('should be able to split a match into parts', () => {
+    const result = test.getMatchParts('amcsi\\playground\\test\\Class6Test::test3 with data set #3');
+    const expected = {
+      _class: 'amcsi\\playground\\test\\Class6Test::',
+      method: 'test3',
+      dataset: ' with data set #3',
+    };
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should ignore dataset if there is none', () => {
+    const result = test.getMatchParts('amcsi\\playground\\test\\Class6Test::test3');
+    const expected = {
+      _class: 'amcsi\\playground\\test\\Class6Test::',
+      method: 'test3',
+      dataset: '',
+    };
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should group methods into one if there are more', () => {
+    const methodKeyObject = { method1: [' with data set #1'], method2: [''] };
+    const result = test.groupMethods(methodKeyObject);
+    const expected = '(method1 with data set #1|method2)';
+    expect(result).to.equal(expected);
+  });
+
+  it('should group tests together as long as datasets are not involved', () => {
+    const result = convertMatchesToPhpStormArgument(getMatches(groupedExample));
+    const expected = '--filter' +
+      ' "/amcsi\\\\playground\\\\test\\\\Class6Test::(test1|test2)$/"';
+    expect(result).to.equal(expected);
+  });
+
   it('should show the correct PhpStorm test runner options', () => {
     const result = convertMatchesToPhpStormArgument(getMatches(bigExample));
-    const expected = '--filter="/^(amcsi\\\\playground\\\\test\\\\Class3Test::testGetLargestPrimeFactor|amcsi\\\\playground\\\\test\\\\Class4Test::test1|amcsi\\\\playground\\\\test\\\\Class4Test::test2|amcsi\\\\playground\\\\test\\\\Class4Test::test3|amcsi\\\\playground\\\\test\\\\Class5Test::test1|amcsi\\\\playground\\\\test\\\\Class5Test::test2|amcsi\\\\playground\\\\test\\\\Class5Test::test3|amcsi\\\\playground\\\\test\\\\Class6Test::test1|amcsi\\\\playground\\\\test\\\\Class6Test::test2|amcsi\\\\playground\\\\test\\\\Class6Test::test3|amcsi\\\\playground\\\\test\\\\Class7Test::test1|amcsi\\\\playground\\\\test\\\\Class7Test::test2|amcsi\\\\playground\\\\test\\\\Class7Test::test3|amcsi\\\\playground\\\\test\\\\Class8Test::test1|amcsi\\\\playground\\\\test\\\\Class8Test::test2|amcsi\\\\playground\\\\test\\\\Class8Test::test3|amcsi\\\\playground\\\\test\\\\Class9Test::test1|amcsi\\\\playground\\\\test\\\\Class9Test::test2|amcsi\\\\playground\\\\test\\\\Class9Test::test3)/"'
+    const expected = '--filter "/(amcsi\\\\playground\\\\test\\\\Class3Test::testGetLargestPrimeFactor with data set #3|amcsi\\\\playground\\\\test\\\\Class4Test::(test1|test2|test3)|amcsi\\\\playground\\\\test\\\\Class5Test::(test1|test2|test3)|amcsi\\\\playground\\\\test\\\\Class6Test::(test1|test2|test3)|amcsi\\\\playground\\\\test\\\\Class7Test::(test1|test2|test3)|amcsi\\\\playground\\\\test\\\\Class8Test::(test1|test2|test3)|amcsi\\\\playground\\\\test\\\\Class9Test::(test1|test2|test3))$/"';
     expect(result).to.equal(expected);
   });
 });
